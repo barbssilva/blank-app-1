a7253e86ee4811db8b67e1dc7f64b0ecbb3dddbb
barbssilva <barbarasilva@anglotex.pt>
2025-11-05 15:09:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ summary_files = st.file_uploader(
     key="uploader_summary"
 )
 
-# Pequena ajuda visual
+# para visualizar os ficheiros que foram carregados
 col1, col2 = st.columns(2)
 with col1:
     st.caption("Standard")
@@ -33,3 +33,36 @@ with col1:
 with col2:
     st.caption("Summary")
     st.write([f.name for f in (summary_files or [])])
+
+
+if standard_files is not None:
+    standard_temp_paths = []  # aqui guardas o caminho de cada ficheiro temporário
+    for f in standard_files:
+        # cria um ficheiro temporário com a mesma extensão
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_excel:
+            # guarda o conteúdo do ficheiro carregado
+            temp_excel.write(f.read())
+            # guarda o caminho
+            standard_temp_paths.append(Path(temp_excel.name))
+    #obter o diretorio do ficheiro temporário:
+    temp_dir_standard = os.path.dirname(standard_temp_paths[0])
+    output_file_standard = os.path.join(temp_dir,'STANDARD_PL.xlsx')
+    
+        
+if summary_files is not None:
+    summary_temp_paths = []  # aqui guardas o caminho de cada ficheiro temporário
+    for f in summary_files:
+        # cria um ficheiro temporário com a mesma extensão
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_excel:
+            # guarda o conteúdo do ficheiro carregado
+            temp_excel.write(f.read())
+            # guarda o caminho
+            summary_temp_paths.append(Path(temp_excel.name))
+    #obter o diretorio do ficheiro temporário:
+    temp_dir_summary = os.path.dirname(summary_temp_paths[0])
+    output_file_summary = os.path.join(temp_dir,'SUMMARY_PL.xlsx')
+    last_file = os.path.join(temp_dir,'Standard and Summary PACKING LIST.xlsx')
+
+placeholder = st.empty()
+placeholder.info("⏳ Por favor aguarde...")
+    
