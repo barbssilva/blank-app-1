0f74e24772574397de52f5dfdbb4f57dd5c80db4
barbssilva <barbarasilva@anglotex.pt>
2025-11-05 15:44:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,33 +56,34 @@ if standard_files:
     output_file_standard = os.path.join(temp_dir_standard,'STANDARD_PL.xlsx')
     
         
-    if summary_files:
-        summary_temp_paths = []  # aqui guardas o caminho de cada ficheiro temporário
-        for f in summary_files:
-            # cria um ficheiro temporário com a mesma extensão
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_excel:
-                # guarda o conteúdo do ficheiro carregado
-                temp_excel.write(f.read())
-                # guarda o caminho
-                summary_temp_paths.append(Path(temp_excel.name))
-        #obter o diretorio do ficheiro temporário:
-        temp_dir_summary = summary_temp_paths[0].parent
-        output_file_summary = os.path.join(temp_dir_summary,'SUMMARY_PL.xlsx')
-        last_file = os.path.join(temp_dir_summary,'Standard and Summary PACKING LIST.xlsx')
-    
+if summary_files:
+    summary_temp_paths = []  # aqui guardas o caminho de cada ficheiro temporário
+    for f in summary_files:
+        # cria um ficheiro temporário com a mesma extensão
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_excel:
+            # guarda o conteúdo do ficheiro carregado
+            temp_excel.write(f.read())
+            # guarda o caminho
+            summary_temp_paths.append(Path(temp_excel.name))
+    #obter o diretorio do ficheiro temporário:
+    temp_dir_summary = summary_temp_paths[0].parent
+    output_file_summary = os.path.join(temp_dir_summary,'SUMMARY_PL.xlsx')
+    last_file = os.path.join(temp_dir_summary,'Standard and Summary PACKING LIST.xlsx')
+
+if summary_files and standard_files:
     placeholder = st.empty()
     placeholder.info("⏳ Por favor aguarde...")
-    
+        
     standard_pl=join_excels(standard_temp_paths,'standard', output_file_standard)
     summary_pl=join_excels(summary_temp_paths,'summary', output_file_summary)
-    
+        
     join_pls(summary_pl,standard_pl,last_file)
-    
+        
     remove_pls(standard_pl,summary_pl)
-    
+        
     placeholder.empty()
     st.success("Processo terminado!")
-            
+                
     # Abrir o ficheiro Excel processado para download
     with open(excel_saida, "rb") as f:
         st.download_button("Descarregar Excel Processado", f, file_name=os.path.basename(last_file))
